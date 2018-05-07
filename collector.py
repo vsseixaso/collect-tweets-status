29ead0182a79aa7fae7009570dbbcbe19aa7126b
vsseixaso <seeixas98@gmail.com>
2018-05-07 17:51:45 -0300

    status in json

--- collector.py
+++ collector.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import tweepy
+import json
 
 # authentications
 consumer_key = 'ZwU4lUEzjUIrCIN481bmd82KA'
@@ -10,7 +11,7 @@ access_token_secret = 'o6vHoBczBW4n54d6eJX3EUtDnMovXXnPFJfd9AjjTM5HF'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 # reading Twitter.txt and fill matters
 matters = []
@@ -21,11 +22,12 @@ for line in file:
 	aux[0] = aux[0][4:]
 	aux[1] = aux[1][6:]
 	matters.append(aux)
+file.close()
 
 # collect tweets status
 for matter in matters:
 	for i in xrange(2, len(matter)):
 		try:
-			print api.get_status(matter[i]).text
+			print json.dumps(api.get_status(matter[i]))
 		except tweepy.error.TweepError:
 			print "TweepError: Tweet indisponível"
